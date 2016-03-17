37f31527a73341c775b7d6d23299f924f1c2cf4c
Ubuntu <ubuntu@ip-172-31-15-229.ec2.internal>
2016-03-17 04:26:51 +0000

    udpate

---
+++ jsonbackup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+while true 
+do
+    date=$(date +"%m_%d_%Y_%s")
+    dirname='dump'.date
+    # dump= mongodump --port 4001 --db safir --collection colname
+    if [ -d "dirname" ]
+    then
+    	echo "folder is already work!!!"
+    else
+     	mkdir "$date"
+	cp export.sh $date
+     	cd $date
+     	./export.sh
+	wait
+	rm export.sh
+     	cd ..
+	git add --all
+	wait
+	git commit -m "udpate db"
+	wait
+	sudo git push --repo https://lengsophy:sophyleng1267437611@github.com/lengsophy/jsonbackup.git
+
+    fi
+    sleep 60s
+
+done
